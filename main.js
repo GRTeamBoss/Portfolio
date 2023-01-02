2b367375d25768eda3ca93bed309df2666e8154b
GRTeamBoss <rabbitwow97@gmail.com>
2023-01-03 02:08:35 +0500

    0.2.0

---
+++ main.js
@@ -0,0 +1,15 @@
+function scrollTop() {
+    window.scrollTo({"top": 0, "behavior": "smooth"});
+}
+
+function reloadPage() {
+    location.reload();
+}
+
+function locationMove(name) {
+    location.href = name;
+}
+
+function openSite(name) {
+    window.open(name, '_blank');
+}
