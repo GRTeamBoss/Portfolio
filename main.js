5a1c055dc580ff30a38d8e25201f8a0fd1de7d28
GRTeamBoss <rabbitwow97@gmail.com>
2023-02-11 02:24:48 +0500

    11.02.2023 work

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-function scrollTop() {
-    window.scrollTo({"top": 0, "behavior": "smooth"});
+/**document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+        updateTelegramPosts();
+    }
+};*/
+
+function updateTelegramPosts() {
+    const posts = update();
+    const parent = document.getElementById("posts");
+    for (let item of posts) {
+        parent.innerHTML = item;
+    }
 }
 
-function reloadPage() {
-    location.reload();
-}
-
-function locationMove(name) {
-    location.href = name;
-}
-
-function openSite(name) {
-    window.open(name, '_blank');
+function update() {
+    const link = "https://t.me/s/grteambossCodingChannel/";
+    const start = [
+        "3"
+    ];
+    let posts = [];
+    for (let item of start) {
+        posts.push('<script async type="text/javascript" src="https://telegram.org/js/telegram-widget.js?21" data-telegram-post="grteambossCodingChannel/'+item+'" data-width="100%"></script>');
+    }
+    return posts;
 }
